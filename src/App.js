3d791753967ec8d87bb1aa503a950cca5aab518c
Amir <amirhoosindoosti15@gmail.com>
2023-11-11 21:07:40 +0330

    Added new file's, Patch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+        <TodoWrapper />
     </div>
   );
 }

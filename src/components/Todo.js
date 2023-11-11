7dee06b6ad24f1ed4f72e9d828e6b8143370b8fd
Amir <amirhoosindoosti15@gmail.com>
2023-11-11 22:29:00 +0330

    updated the code from the tutorial video on YT
    I followed the tutorial on https://www.youtube.com/watch?v=LoYbN6qoQHA
    I will be making this code more mine with edits and customization's

---
+++ src/components/Todo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  return (
+    <div className="Todo">
+      <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
+      <div>
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          onClick={() => editTodo(task.id)}
+        />
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          onClick={() => deleteTodo(task.id)}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTodo(value);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        className="todo-input"
+        placeholder="insert a new task"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Add task
+      </button>
+    </form>
+  );
+};
